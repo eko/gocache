b6bc764124a44ec3dddd8ae3b61d2370553809ca
yeqown <yeqown@gmail.com>
2020-08-10 10:06:15 +0800

    feat: clear and modify

---
+++ store_interface.go
@@ -0,0 +1,32 @@
+package xcache
+
+import "github.com/yeqown/gocache/types"
+
+// IStore is the interface for all available stores
+type IStore interface {
+	// Get .
+	Get(key string) ([]byte, error)
+
+	// Set .
+	Set(key string, value interface{}, options *types.StoreOptions) error
+
+	// Delete .
+	Delete(key string) error
+
+	// Invalidate .
+	Invalidate(options types.InvalidateOptions) error
+
+	// Clear .
+	Clear() error
+
+	// GetType .
+	GetType() string
+}
+
+const (
+	_tagPrefix = "gocache_tag_"
+)
+
+func genTagKey(tag string) string {
+	return _tagPrefix + tag
+}
