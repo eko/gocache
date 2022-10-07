314a1c604d0d1b17e33c0b1ed804f6bf30156b77
Matthew Crenshaw <mcrenshaw@clearstreet.io>
2022-10-07 14:15:53 -0400

    fix for applyOptionsWithDefault not keeping unmodified options

--- store/options_test.go
+++ store/options_test.go
@@ -36,3 +36,17 @@ func TestOptionsTagsValue(t *testing.T) {
 	// When - Then
 	assert.Equal(t, []string{"tag1", "tag2", "tag3"}, options.tags)
 }
+
+func Test_applyOptionsWithDefault(t *testing.T) {
+	// Given
+	defaultOptions := &Options{
+		expiration: 25 * time.Second,
+	}
+
+	// When
+	options := applyOptionsWithDefault(defaultOptions, WithCost(7))
+
+	// Then
+	assert.Equal(t, int64(7), options.cost)
+	assert.Equal(t, 25*time.Second, options.expiration)
+}
